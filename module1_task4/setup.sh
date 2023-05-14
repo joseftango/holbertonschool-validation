acdff4090dd0e552e56c17e45e45affb781afe6d
joseftango <4279@holbertonschool.com>
2023-05-14 11:59:41 +0100

    setup.sh

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get update && apt-get install -y hugo make
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
-dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt-get install ./hugo.deb
+dpkg -s hugo make
 make build
